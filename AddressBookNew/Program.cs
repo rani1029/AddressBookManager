57136d67621bcdbba26685e886054d7023fa8b32
rani1029 <ranig1029@gmail.com>
2021-09-14 17:17:28 +0530

    welcome msg added

---
+++ AddressBookNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to addressBook management");
+        }
+    }
+}
